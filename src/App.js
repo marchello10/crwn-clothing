71f70ec25c902d13baf3ba6e6467bf513850a3d5
marchello10 <martyna.gajauskaite@gmail.com>
2020-10-04 17:56:55 +0300

    updated menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
-import HomePage from './pages/homepage/homepage.component'
+
+import HomePage from './pages/homepage/homepage.component';
+
 
 function App() {
+  //switch - finds one match in a path, does not render anything else
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route path='/' component={HomePage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+// withRouter is a higher order component
+// a function that takes a component as an argument and 
+// returns you a modified component
+
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, imageUrl, size }) => (
+// we have access to history because of export withRouter(MenuItem)
+// we use ${match.url}, so we can access our page even if we write smth like this:
+// http://localhost:3000/somemarchedURL/linkUrl
+
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
     <div
-        className={`${size} menu-item`}
+        className={`${size} menu-item`} 
+        onClick={() => history.push(`${match.url}${linkUrl}`)}
     >
         <div
             style={{
@@ -18,4 +28,5 @@ const MenuItem = ({ title, imageUrl, size }) => (
     </div>
 )
 
-export default MenuItem;
+// return components with access to location, match and history props
+export default withRouter(MenuItem);
